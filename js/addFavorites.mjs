464d239dca54c89161fb1ce747adb6c64992433d
Frontend-Freak <slipknot.rulit3@gmail.com>
2025-06-18 21:13:05 +0300

    commit

--- js/addFavorites.mjs
+++ js/addFavorites.mjs
@@ -4,11 +4,11 @@ export const addFavoritesBtn = document.getElementById("addToFavoritesBtn");
 let isFavorites = false;
 
 export function addFavorites() {
-	if (isFavorites) {
+	if (!isFavorites) {
 		addFavoritesBtn.className = "added";
-		isFavorites = false;
+		isFavorites = true;
 	} else {
 		addFavoritesBtn.classList.remove("added");
-		isFavorites = true;
+		isFavorites = false;
 	}
 }
